9bd73c277a55a781ac5b493525ad6f286eefa12a
lukavr05 <lvrooyen@yahoo.com>
2024-10-09 12:09:52 +0100

    added custom password-length

--- ImageEncrypter.java
+++ ImageEncrypter.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 class ImageEncrypter {
     public static void main(String[] args) throws IOException {
         String path = args[0];
-        Encrypter encrypter = new Encrypter(path);
-        
+               
         Scanner scan = new Scanner(System.in);
         System.out.println("Would you like to select:\n\n1) Password Creation\n\n2) Password Encryption\n\n3) Password Decryption\n\n");
         
         int choice = scan.nextInt();
-        scan.close();
 
         if (choice == 1) {
+            System.out.println("Specify desired password length: ");
+            int l = scan.nextInt();
+
+            Encrypter encrypter = new Encrypter(path, l);
             String password = encrypter.generateInputSeed();
             System.out.println("Your password is: " + password);
+
         } else if (choice == 2){
             System.out.println("Enter password to be encrypted: ");
         }  
     
     }
+}
 
